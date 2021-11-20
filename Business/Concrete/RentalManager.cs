9405795d80dfd50d356534124bd44df7f40f9b34
Emre Aka <aka548548@gmail.com>
2021-11-21 00:10:51 +0300

    Cache aspects are added.

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Business.Abstract;
+using Core.Aspects.Autofac.Caching;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entity.Concrete;
@@ -16,16 +17,19 @@ namespace Business.Concrete
             _rentalDal = rentalDal;
         }
 
+        [CacheAspect]
         public IDataResult<List<Rental>> GetAll()
         {
             return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
         }
 
+        [CacheAspect]
         public IDataResult<Rental> GetById(int id)
         {
             return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.Id == id));
         }
 
+        [CacheRemoveAspect("IRentalService.Get")]
         public IResult Add(Rental rental)
         {
             Rental result =  _rentalDal.Get(r => r.CarId == rental.CarId && r.ReturnDate == null);
@@ -37,18 +41,21 @@ namespace Business.Concrete
             return new SuccessResult("Rental added");
         }
 
+        [CacheRemoveAspect("IRentalService.Get")]
         public IResult Delete(Rental rental)
         {
             _rentalDal.Delete(rental);
             return new SuccessResult("Rental deleted");
         }
 
+        [CacheRemoveAspect("IRentalService.Get")]
         public IResult Update(Rental rental)
         {
             _rentalDal.Delete(rental);
             return new SuccessResult("Rental updated");
         }
 
+        [CacheAspect]
         public IDataResult<List<RentalDetailDto>> GetDetails()
         {
             return new SuccessDataResult<List<RentalDetailDto>>(_rentalDal.GetRentalDetails());
