8c6d8e5af3a7d6dafae6fe834ce4496204782d96
Emre Aka <aka548548@gmail.com>
2022-02-20 17:00:06 +0300

    Register method is connected to Customer Service.

---
+++ Entity/DTOs/CustomerForRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Entity.DTOs
+{
+    public class CustomerForRegisterDto: IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? CompanyName { get; set; }
+    }
+}
