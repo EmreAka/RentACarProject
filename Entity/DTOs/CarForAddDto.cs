c9ca84b81d4ae15ea7e32926c9a80a597fa789f6
Emre Aka <aka548548@gmail.com>
2022-05-17 20:05:19 +0300

    Car Entity is updated.

--- Entity/DTOs/CarForAddDto.cs
+++ Entity/DTOs/CarForAddDto.cs
@@ -1,24 +1,24 @@
-﻿using Core.Entities;
+﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entity.Concrete
+namespace Entity.DTOs
 {
-    public class Car:IEntity
+    public class CarForAddDto
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public int BrandId { get; set; }
         public int ColourId { get; set; }
         public int EngineId { get; set; }
         public int FuelId { get; set; }
-        public int FuelConsumption { get; set; }
-        public int DoorNumber { get; set; }
-        public int UserId { get; set; }
         public int ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
+        public int FuelConsumption { get; set; }
+        public int DoorNumber { get; set; }
         public string Description { get; set; }
+        public List<IFormFile> Images { get; set; }
     }
 }
