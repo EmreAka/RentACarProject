364e5d4e14dfa8238e46dbdf775a79ff3f8daf54
Emre Aka <aka548548@gmail.com>
2022-05-23 00:13:02 +0300

    DTO for rental.

---
+++ Entity/DTOs/RentalWithCardInfoDto.cs
@@ -0,0 +1,10 @@
+﻿using Entity.Concrete;
+using Card = Core.Entities.Concrete.Card;
+
+namespace Entity.DTOs;
+
+public class RentalWithCardInfoDto
+{
+    public Rental Rental { get; set; }
+    public Card Card { get; set; }   
+}
